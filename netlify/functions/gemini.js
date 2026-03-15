0b069f4db96f9042e124564d43201c2f01f0e80a
Adrin Caballero <aldrin.caballero@orangeapps.ph>
2026-03-15 11:01:43 +0800

    Updated

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -61,7 +61,7 @@ exports.handler = async function (event) {
   const safePropmt = prompt.slice(0, 8000);
 
   const geminiRes = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
